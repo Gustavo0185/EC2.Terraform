f30c8547aa266ec8b99dbd43d3770dc667e87b3d
Gustavo Antunes de Souza <130326721+Gustavo0185@users.noreply.github.com>
2023-11-27 14:15:24 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region"{
+    type = string
+    description = ""
+    default = "us-east-1"
+}
+
+variable "instance_ami"{
+    type = string
+    description = ""
+    default = "ami-0230bd60aa48260c6"
+}
+
+variable "instance_type"{
+    type = string
+    description = ""
+    default = "t3.micro"
+}
+
+variable "instance_tags" {
+  type        = map(string)
+  description = ""
+
+  default = {
+    Name    = "linuxGustavo"
+    Project = "awsterraform"
+  }
+}
+
+
+
